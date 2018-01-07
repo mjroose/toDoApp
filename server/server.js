e2e36bd1c7024ca20599811e7a42d01185430230
mjroose <mjroose@gmail.com>
2018-01-07 12:35:33 -0500

    Added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -9,6 +10,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
+// POST /todos route - adds a new todo to database
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
@@ -21,13 +23,31 @@ app.post('/todos', (req, res) => {
     });
 });
 
+// GET /todos route - returns all todos from database
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
     }, (e) => {
         res.status(404).send(e);
     });
-})
+});
+
+// GET /todos/:id route - finds a single todo by id
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (todo) {
+            res.send({todo});
+        } else res.status(404).send();
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
