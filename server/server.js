f6b46c8fe3cc3acdc78d333613a02ea3a9e67fca
mjroose <mjroose@gmail.com>
2018-01-07 19:05:53 -0500

    Added DELETE /todos/:id route and corresponding test

--- server/server.js
+++ server/server.js
@@ -50,6 +50,24 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+// DELETE todo
+
+app.delete('/todos/:id', (req, res) => {
+    //get the id
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) return res.status(404).send();
+    
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) return res.status(404).send();
+        
+        res.send({todo});
+            
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
