82351e777b687fa8b9e8fa2491de92eee760566b
mjroose <mjroose@gmail.com>
2018-02-05 15:33:10 -0500

    Convert all remaining async routes to use async/await

--- server/server.js
+++ server/server.js
@@ -15,48 +15,50 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.post('/todos', authenticate, (req, res) => {
-  var todo = new Todo({
+app.post('/todos', authenticate, async (req, res) => {
+  const todo = new Todo({
     text: req.body.text,
     _creator: req.user._id
   });
 
-  todo.save().then((doc) => {
+  try {
+    const doc = await todo.save();
     res.send(doc);
-  }, (e) => {
+  } catch (e) {
     res.status(400).send(e);
-  });
-});
-
-app.get('/todos', authenticate, (req, res) => {
-  Todo.find({
-    _creator: req.user._id
-  }).then((todos) => {
-    res.send({todos});
-  }, (e) => {
-    res.status(400).send(e);
-  });
-});
-
-app.get('/todos/:id', authenticate, (req, res) => {
-  var id = req.params.id;
-
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send();
   }
+});
 
-  Todo.findOne({
-      _id: id,
-      _creator: req.user._id
-    }).then((todo) => {
+app.get('/todos', authenticate, async (req, res) => {
+  try { 
+    const todos = await Todo.find({_creator: req.user._id});
+    res.send({todos});
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.get('/todos/:id', authenticate, async (req, res) => {
+  try {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+      return res.status(404).send();
+    }
+  
+    const todo = await Todo.findOne({
+        _id: id,
+        _creator: req.user._id
+      });
+      
     if (!todo) {
       return res.status(404).send();
     }
 
     res.send({todo});
-  }).catch((e) => {
+  } catch (e) {
     res.status(400).send();
-  });
+  }
 });
 
 app.delete('/todos/:id', authenticate, async (req, res) => {
@@ -83,33 +85,35 @@ app.delete('/todos/:id', authenticate, async (req, res) => {
   }
 });
 
-app.patch('/todos/:id', authenticate, (req, res) => {
-  var id = req.params.id;
-  var body = _.pick(req.body, ['text', 'completed']);
-
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send();
-  }
-
-  if (_.isBoolean(body.completed) && body.completed) {
-    body.completedAt = new Date().getTime();
-  } else {
-    body.completed = false;
-    body.completedAt = null;
-  }
-
-  Todo.findOneAndUpdate({
-    _id: id,
-    _creator: req.user._id
-  }, {$set: body}, {new: true}).then((todo) => {
+app.patch('/todos/:id', authenticate, async (req, res) => {
+  try {
+    const id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+  
+    if (!ObjectID.isValid(id)) {
+      return res.status(404).send();
+    }
+  
+    if (_.isBoolean(body.completed) && body.completed) {
+      body.completedAt = new Date().getTime();
+    } else {
+      body.completed = false;
+      body.completedAt = null;
+    }
+  
+    const todo = await Todo.findOneAndUpdate({
+      _id: id,
+      _creator: req.user._id
+    }, {$set: body}, {new: true});
+    
     if (!todo) {
       return res.status(404).send();
     }
-
+  
     res.send({todo});
-  }).catch((e) => {
+  } catch (e) {
     res.status(400).send();
-  })
+  }
 });
 
 // POST /users
