84f4fe5381316c09fe792c7dd80e311686afcaa2
mjroose <mjroose@gmail.com>
2018-02-09 21:12:22 -0500

    Expose x-auth header to client app

--- server/server.js
+++ server/server.js
@@ -15,8 +15,9 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Expose-Headers', 'x-auth');
   next();
 });
 
